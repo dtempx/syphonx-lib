93a8f12f9511b557c6b0a26fe2a7d0ef14d35043
Dave Templin <dave.templin@pricespider.com>
2024-05-01 12:47:51 -0700

    fix cjs dist

--- test/fetch/1.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai";
-import { fetchTemplate } from "../../template.js";
-import { Template } from "syphonx-core";
-
-describe("fetch/1", () => {
-    let template: Template | undefined = undefined;
-    before(async () => template = await fetchTemplate("$/examples/example.json"));
-    it("actions has expected length", () => expect(template?.actions).to.have.length(1));
-});
